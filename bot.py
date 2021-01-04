b5ca26bd6ad7aec8f339b7cd5ac0e160477a7adb
InfWolf14 <infsutter14@gmail.com>
2021-01-04 16:03:00 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def get_prefix(bot, message):
 
 # Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
 # Think of it like a dot path import
-initial_cogs = ['ironworks']
+initial_cogs = ['master', 'cogs.ironworks']
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=get_prefix, description='Wolf\'s Child', intents=intents)
