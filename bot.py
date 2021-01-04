4ab67fe3ec9c0489db63104edd8e85a48cea596c
InfWolf14 <infsutter14@gmail.com>
2021-01-04 13:05:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,60 +5,58 @@ import logging
 from discord.ext import commands
 from database.database import Postgres
 
+# ==============================================================================
+# ==============================================================================
 
 def get_prefix(bot, message):
-    """a callable prefix for our bot which can be customized by each guild"""
+    default_prefix = 'wp;'
 
-    # set default prefix
-    default_prefix = ';'
-
-    # check if we're outside a guild (like in a DM, etc.)
     if not message.guild:
-        # only allow the default prefix or mention to be used in DMs
         return commands.when_mentioned_or(*default_prefix)(bot, message)
 
     with Postgres() as db:
-        # check if this guild has a custom prefix
         prefix = db.query('SELECT prefix FROM prefixes WHERE guild_id = %s', (message.guild.id,))
 
-        # if so, return that prefix
         if prefix:
             return commands.when_mentioned_or(str(*prefix[0]))(bot, message)
-
-    # otherwise return the default prefix
     return commands.when_mentioned_or(*default_prefix)(bot, message)
 
+# ==============================================================================
 
-# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
-# Think of it like a dot path import
-initial_cogs = ['cogs.starboard', 'cogs.owner', 'cogs.wishwall']
+initial_cogs = ['cogs.ironworks']
 
-bot = commands.Bot(command_prefix=get_prefix, description='A bot for GoldxGuns')
+# ==============================================================================
+
+bot = commands.Bot(command_prefix=get_prefix, description='Wolf's Child')
 
 
 @bot.command(name='ping')
 async def ping(ctx):
-    """returns the bot's latency in milliseconds"""
-
-    # return ping
     await ctx.send(f':ping_pong:⠀⠀**Pong!**⠀{round(bot.latency, 3)}ms')
 
+@bot.command(name='server')
+async def server(ctx):
+    await ctx.send("This command isn't ready yet!")
 
+@bot.command(name='credits')
+async def credits(ctx):
+    embed = discord.Embed(title='Development Credits', description='...', colour=0xffff33)
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    await ctx.send(content=None, embed=embed)
+                   
 @bot.group(name='prefix', pass_context=True)
 async def prefix(ctx):
-    """returns the bot's prefix for this guild"""
-
     # check that we're not in a subcommand
     if ctx.invoked_subcommand is None:
-        # return prefix
-        await ctx.send(f'My prefix is `{get_prefix(bot, ctx.message)[2]}`')
-
+        await ctx.send(f'Current prefix set to: `{get_prefix(bot, ctx.message)[2]}`')
 
 @prefix.command(name='set', no_pm=True, hidden=True, pass_context=True, aliases=['change'])
+                   
 @commands.has_guild_permissions(administrator=True)
+                   
+# ==============================================================================
+                   
 async def set(ctx, new_prefix):
-    """changes the bot's prefix for this guild"""
-
     # instantiate database
     with Postgres() as db:
         # check if there's already an entry for this guild
@@ -76,25 +74,9 @@ async def set(ctx, new_prefix):
             await ctx.send(f':white_check_mark:⠀⠀**Success!**⠀my prefix is now `{new_prefix}`')
         # else something went wrong
         else:
-            await ctx.send('<:wrong:742105251271802910>⠀⠀**Error!!**⠀my prefix couldn\'t be changed because something went wrong')
-
-
-@bot.command(name='server')
-async def server(ctx):
-    await ctx.send("This command isn't ready yet!")
-
-
-@bot.command(name='credits')
-async def credits(ctx):
-    embed = discord.Embed(title='Development Credits', description='Thank you to all of the following folks for making Gigi possible.', colour=0xffff33)
-    embed.set_thumbnail(url=bot.user.avatar_url)
-    embed.add_field(name='Lead Developer', value='@ShiinaBat#8227', inline=True)
-    embed.add_field(name='Developers', value='@Adam.M#9788\n@Element#1337', inline=True)
-    embed.add_field(name='Alpha Testers', value='@AzureEiyu#9781, @CursedQuill#5719, @Isaac2K#1948, @Kurokaito#5489, @Vyxea#0001', inline=False)
-    embed.add_field(name='Character Designer', value='NEBULArobo ([https://nebularobo.carrd.co/](https://nebularobo.carrd.co/))', inline=False)
-    embed.add_field(name='Icon Artist', value='crankiereddy ([https://twitter.com/crankiereddy](https://twitter.com/crankiereddy))', inline=False)
-    await ctx.send(content=None, embed=embed)
-
+            await ctx.send('<:wrong:742105251271802910>⠀⠀**Error!!**⠀my prefix couldn\'t be changed because of something Kaito did...')
+                   
+# ==============================================================================
 
 # Here we load our extensions(cogs) listed above in [initial_extensions].
 if __name__ == '__main__':
