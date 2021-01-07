b127967e0e0bf9706c9f1d4218a305bc19a76bf1
InfWolf14 <infsutter14@gmail.com>
2021-01-07 01:47:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,29 +3,19 @@ from discord.ext import commands
 
 
 def get_prefix(bot, message):
-    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
-
     prefixes = ['w^']
-
-    # Check to see if we are outside of a guild. e.g DM's etc.
     if not message.guild:
-        # Only allow ? to be used in DMs
         return 'w^'
-
-    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 
-# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
-# Think of it like a dot path import
-initial_cogs = ['master', 'cogs.ironworks']
+initial_cogs = ['util', 'cogs.ironworks']
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=get_prefix, description='Wolf\'s Child', intents=intents)
-
-# Here we load our extensions(cogs) listed above in [initial_extensions].
 if __name__ == '__main__':
     for extension in initial_cogs:
+        print('Loading '+str(extension))
         bot.load_extension(extension)
 
 
