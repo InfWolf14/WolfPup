22a2e5d51a4d9117952b9046217c58a6a66e0ffe
InfWolf14 <infsutter14@gmail.com>
2021-01-07 01:48:37 -0500

    Update and rename master.py to util.py

--- util.py
+++ util.py
@@ -1,53 +1,47 @@
 from discord.ext import commands
 
 
-class MasterCog(commands.Cog, name='master'):
-
+class UtilCog(commands.Cog, name='util'):
     def __init__(self, bot):
         self.bot = bot
 
-    # Hidden means it won't show up on the default help.
+    @commands.command(name='ping', hidden=True)
+    @commands.is_owner()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong')
+
     @commands.command(name='load', hidden=True)
     @commands.is_owner()
     async def load(self, ctx, *, cog: str):
-        """Command which Loads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-
         try:
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'Failed to load module: {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'Successfully loaded module')
 
     @commands.command(name='unload', hidden=True)
     @commands.is_owner()
     async def unload(self, ctx, *, cog: str):
-        """Command which Unloads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-
         try:
             self.bot.unload_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'Failed to load module: {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'Successfully loaded module')
 
     @commands.command(name='reload', hidden=True)
     @commands.is_owner()
     async def reload(self, ctx, *, cog: str):
-        """Command which Reloads a Module.
-        Remember to use dot path. e.g: cogs.owner"""
-
         try:
             self.bot.unload_extension(cog)
             self.bot.load_extension(cog)
         except Exception as e:
-            await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
+            await ctx.send(f'Failed to load module: {type(e).__name__} - {e}')
         else:
-            await ctx.send('**`SUCCESS`**')
+            await ctx.send(f'Successfully loaded module')
 
 
 def setup(bot):
-    bot.add_cog(MasterCog(bot))
+    bot.add_cog(UtilCog(bot))
 
